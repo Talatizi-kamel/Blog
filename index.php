76e4d5ffe898394184fc700b01e03bd5c4bb09dc
talatizi kamel <kamel.talatizi@alumni.univ-avignon.fr>
2023-08-21 20:19:01 +0200

    Filtrer les articles  par catégorie

--- index.php
+++ index.php
@@ -1,33 +1,87 @@
 <?php
 $filename = __DIR__ . '/data/article.json';
 $articles = [];
+$categories = [];
+
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$selectedCat = $_GET['cat'] ?? '';
 
 if (file_exists($filename)) {
   $articles = json_decode(file_get_contents($filename), true) ?? [];
+  $cattmp = array_map(fn ($a) => $a['category'],  $articles);
+  $categories = array_reduce($cattmp, function ($acc, $cat) {
+    if (isset($acc[$cat])) {
+      $acc[$cat]++;
+    } else {
+      $acc[$cat] = 1;
+    }
+    return $acc;
+  }, []);
+  $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+    if (isset($acc[$article['category']])) {
+      $acc[$article['category']][] = $article;
+    } else {
+      $acc[$article['category']] = [$article];
+    }
+    return $acc;
+  }, []);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-      <?php require_once 'include/head.php' ?>
-      <link rel="stylesheet" href="/public/css/index.css">
-      <title>Blog</title>
+    <?php require_once 'include/head.php' ?>
+    <link rel="stylesheet" href="/public/css/index.css">
+    <title>Blog</title>
   </head>
 
   <body>
     <div class="container">
       <?php require_once 'include/header.php' ?>
       <div class="content">
-        <div class="articles-container">
-          <?php foreach ($articles as $a) : ?>
-            <div class="article block">
-              <div class="overflow">
-                <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+        <div class="newsfeed-container">
+          <div class="category-container">
+              <ul class="category-container">
+                  <li class=<?= $selectedCat ? '' : 'cat-active' ?>>
+                      <a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a>
+                  </li>
+                  <?php foreach ($categories as $catName => $catNum) : ?>
+                      <li class=<?= $selectedCat ===  $catName ? 'cat-active' : '' ?>>
+                          <a href="/?cat=<?= $catName ?>"> <?= $catName ?><span class="small">(<?= $catNum ?>)</span> </a>
+                      </li>
+                  <?php endforeach; ?>
+              </ul>
+          </div>
+          <div class="newsfeed-content">
+            <?php if (!$selectedCat) : ?>
+              <?php foreach ($categories as $cat => $num) : ?>
+              <h2><?= $cat ?></h2>
+              <div class="articles-container">
+                <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                  <div class="article block">
+                    <div class="overflow">
+                      <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                    </div>
+                    <h3><?= $a['title'] ?></h3>
+                  </div>
+                <?php endforeach; ?>
               </div>
-              <h2><?= $a['title'] ?></h2>
-            </div>
-          <?php endforeach; ?>
+            <?php endforeach; ?>
+          <?php else : ?>
+            <h2><?= $selectedCat ?></h2>
+            <div class="articles-container">
+                <?php foreach ($articlePerCategories[$selectedCat] as $a) : ?>
+                  <div class="article block">
+                    <div class="overflow">
+                      <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                    </div>
+                    <h3><?= $a['title'] ?></h3>
+                  </div>
+                <?php endforeach; ?>
+              </div>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
       <?php require_once 'include/footer.php' ?>
